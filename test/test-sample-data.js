40fa42f79bbc76c47c90e8abab2ef80254e16a9a
Scott Brabson <scottbrabson@gmail.com>
2020-10-25 17:26:14 -0600

    Create test-data file with four data sets. export to all TDD tests

---
+++ test/test-sample-data.js
@@ -0,0 +1,279 @@
+export const activitySampleData = [
+  {
+    "userID": 33,
+    "date": "2020/06/01",
+    "numSteps": 3577,
+    "minutesActive": 140,
+    "flightsOfStairs": 16
+  },
+  {
+    "userID": 33,
+    "date": "2020/06/02",
+    "numSteps": 4294,
+    "minutesActive": 138,
+    "flightsOfStairs": 10
+  },
+  {
+    "userID": 33,
+    "date": "2020/06/03",
+    "numSteps": 7402,
+    "minutesActive": 116,
+    "flightsOfStairs": 33
+  },
+  {
+    "userID": 13,
+    "date": "2020/06/01",
+    "numSteps": 3486,
+    "minutesActive": 114,
+    "flightsOfStairs": 32
+  },
+  {
+    "userID": 13,
+    "date": "2020/06/02",
+    "numSteps": 11374,
+    "minutesActive": 213,
+    "flightsOfStairs": 13
+  },
+  {
+    "userID": 13,
+    "date": "2020/06/03",
+    "numSteps": 14810,
+    "minutesActive": 287,
+    "flightsOfStairs": 18
+  },
+  {
+    "userID": 29,
+    "date": "2020/06/01",
+    "numSteps": 2634,
+    "minutesActive": 107,
+    "flightsOfStairs": 5
+  },
+  {
+    "userID": 29,
+    "date": "2020/06/02",
+    "numSteps": 10333,
+    "minutesActive": 114,
+    "flightsOfStairs": 31
+  },
+  {
+    "userID": 29,
+    "date": "2020/06/03",
+    "numSteps": 6389,
+    "minutesActive": 41,
+    "flightsOfStairs": 33
+  },
+  {
+    "userID": 9,
+    "date": "2020/06/01",
+    "numSteps": 2634,
+    "minutesActive": 107,
+    "flightsOfStairs": 5
+  },
+  {
+    "userID": 9,
+    "date": "2020/06/02",
+    "numSteps": 10333,
+    "minutesActive": 114,
+    "flightsOfStairs": 31
+  },
+  {
+    "userID": 9,
+    "date": "2020/06/03",
+    "numSteps": 6389,
+    "minutesActive": 41,
+    "flightsOfStairs": 33
+  }
+]
+
+export const hydrationSampleData = [
+  {
+    "userID": 33,
+    "date": "2020/06/01",
+    "numOunces": 37
+  },
+  {
+    "userID": 33,
+    "date": "2020/06/15",
+    "numOunces": 75
+  },
+  {
+    "userID": 33,
+    "date": "2020/06/15",
+    "numOunces": 47
+  },
+  {
+    "userID": 13,
+    "date": "2020/06/01",
+    "numOunces": 85
+  },
+  {
+    "userID": 13,
+    "date": "2020/06/15",
+    "numOunces": 42
+  },
+  {
+    "userID": 13,
+    "date": "2020/06/15",
+    "numOunces": 87
+  },
+  {
+    "userID": 29,
+    "date": "2020/06/01",
+    "numOunces": 94
+  },
+  {
+    "userID": 29,
+    "date": "2020/06/15",
+    "numOunces": 84
+  },
+  {
+    "userID": 29,
+    "date": "2020/06/15",
+    "numOunces": 39
+  },
+  {
+    "userID": 9,
+    "date": "2020/06/01",
+    "numOunces": 94
+  },
+  {
+    "userID": 9,
+    "date": "2020/06/15",
+    "numOunces": 84
+  },
+  {
+    "userID": 9,
+    "date": "2020/06/15",
+    "numOunces": 39
+  }
+]
+
+export const sleepSampleData = [
+  {
+    "userID": 33,
+    "date": "2020/06/01",
+    "hoursSlept": 6,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 33,
+    "date": "2020/10/16",
+    "hoursSlept": 7,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 33,
+    "date": "2020/10/17",
+    "hoursSlept": 10.8,
+    "sleepQuality": 4.7
+  },
+  {
+    "userID": 13,
+    "date": "2020/06/01",
+    "hoursSlept": 8.1,
+    "sleepQuality": 3.5
+  },
+  {
+    "userID": 13,
+    "date": "2020/06/02",
+    "hoursSlept": 8.9,
+    "sleepQuality": 2.2
+  },
+  {
+    "userID": 13,
+    "date": "2020/07/03",
+    "hoursSlept": 4.4,
+    "sleepQuality": 1.6
+  },
+  {
+    "userID": 29,
+    "date": "2020/06/01",
+    "hoursSlept": 4.6,
+    "sleepQuality": 2.9
+  },
+  {
+    "userID": 29,
+    "date": "2020/06/05",
+    "hoursSlept": 6.1,
+    "sleepQuality": 3.5
+  },
+  {
+    "userID": 29,
+    "date": "2020/06/06",
+    "hoursSlept": 4.7,
+    "sleepQuality": 4
+  },
+  {
+    "userID": 9,
+    "date": "2020/06/01",
+    "hoursSlept": 4.6,
+    "sleepQuality": 2.9
+  },
+  {
+    "userID": 9,
+    "date": "2020/06/05",
+    "hoursSlept": 6.1,
+    "sleepQuality": 3.5
+  },
+  {
+    "userID": 9,
+    "date": "2020/06/06",
+    "hoursSlept": 4.7,
+    "sleepQuality": 4
+  }
+]
+
+export const userSampleData = [
+  {
+  "id": 33,
+  "name": "Scott Brabson",
+  "address": "555 Zuni st, Arvada, CO 80003",
+  "email": "scotty.crackalackin@yahoo.com",
+  "strideLength": 6.6,
+  "dailyStepGoal": 15000,
+  "friends": [
+    29,
+    13,
+    9
+  ]
+},
+{
+  "id": 13,
+  "name": "Bruce Gordyman",
+  "address": "460 Boyd Viaduct, Christchurch NZ 04038-3499",
+  "email": "iAmBatman@hotmail.com",
+  "strideLength": 4.8,
+  "dailyStepGoal": 10000,
+  "friends": [
+    29,
+    9,
+    33
+  ]
+},
+{
+  "id": 29,
+  "name": "Estelle Hoffenhoffer",
+  "address": "3866 Jay Loaf, New Felix OR 96784-0274",
+  "email": "EsTellMeSweetNothings@hotmail.com",
+  "strideLength": 4.4,
+  "dailyStepGoal": 8000,
+  "friends": [
+    9,
+    13,
+    33
+  ]
+},
+{
+  "id": 9,
+  "name": "Bob N Forapples",
+  "address": "Place place, Citycity, MN 09786",
+  "email": "dddd@hotmail.com",
+  "strideLength": 4.2,
+  "dailyStepGoal": 3000,
+  "friends": [
+    29,
+    13,
+    33
+  ]
+}
+];
