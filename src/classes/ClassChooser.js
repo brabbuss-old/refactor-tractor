13fbc32a71d3a3d0b7a26b81fd3e5ea2196a02b5
Scott Brabson <scottbrabson@gmail.com>
2020-10-28 20:16:23 -0600

    Adjust ClassChooser class method for interpolation purposes

--- src/classes/ClassChooser.js
+++ src/classes/ClassChooser.js
@@ -1,18 +1,21 @@
 import Activity from './data-classes/Activity';
 import Hydration from './data-classes/Hydration';
 import Sleep from './data-classes/Sleep';
+import User from './User';
 
 class ClassChooser {
   constructor(className) {
     this.className = className;
   }
   instantiateClass(dataObject) {
-    if (this.className === 'Activity') {
+    if (this.className === 'activities') {
       return new Activity(dataObject)
-    } else if (this.className === 'Sleep') {
+    } else if (this.className === 'sleep') {
       return new Sleep(dataObject)
-    } else if (this.className === 'Hydration') {
+    } else if (this.className === 'hydration') {
       return new Hydration(dataObject)
+    } else if (this.className === 'users') {
+      return new User(dataObject)
     }
   }
 }
