1f6e95e0fa6e4f099c8145c916bd773fe0ea472a
Bruce Gordon <brc.gordon@gmail.com>
2020-11-01 14:51:25 -0700

    Create DOM test file for writing chai spy

--- test/dom-test.js
+++ test/dom-test.js
@@ -1,3 +1,19 @@
-import {expect} from 'chai';
-import flipCard from '../src/scripts';
-import {spies} from 'chai-spies';
+import chai, { expect } from 'chai';
+import spies from 'chai-spies';
+import UserRepository from '../src/classes/UserRepository';
+chai.use(spies);
+
+
+describe('headerName', () => {
+  before(() => {
+
+  });
+
+  it('should call Math.round and Math.random', () => {
+    let Math = {};
+    chai.spy.on(Math, ['round', 'random'], () => {});
+    let data = new UserRepository([], [], [], [], "2020/10/01")
+    data.getRandomUser();
+    expect(Math.random).to.have.been.called(1);
+  });
+})
